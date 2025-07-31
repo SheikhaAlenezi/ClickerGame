e0a800c62b4ec56557d4c9f8f27db0646762cf08
SheikhaAlenezi <shei5ha98@gmail.com>
2025-07-31 20:35:14 +0300

    Project Done

--- app/ShaePlanets.tsx
+++ app/ShaePlanets.tsx
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function ShaePlanets() {
   const [points, setPoints] = useState(0);
+  const [seconds, setSeconds] = useState(0);
 
   const handleClick = () => {
     setPoints((prev) => prev + 10);
   };
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => prevSeconds + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <View style={styles.container}>
+      <Text style={styles.topRightPoints}>Points: {points}</Text>
+
       <Image
         source={{
           uri: "https://static.vecteezy.com/system/resources/previews/024/596/370/non_2x/pink-planet-illustration-free-png.png",
@@ -19,13 +28,13 @@ export default function ShaePlanets() {
       <Text style={styles.title}>Shae Planets</Text>
       <Text style={styles.text}>Points: {points}</Text>
 
+      <Text style={styles.text}>Timer: {seconds} seconds</Text>
       <TouchableOpacity style={styles.button} onPress={handleClick}>
         <Text style={styles.buttonText}>Destroy</Text>
       </TouchableOpacity>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -57,5 +66,16 @@ const styles = StyleSheet.create({
     color: "#1e1e1e",
     fontSize: 16,
     fontWeight: "bold",
+  },
+  topRightPoints: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: " #fa0bc6ff",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 8,
   },
 });
